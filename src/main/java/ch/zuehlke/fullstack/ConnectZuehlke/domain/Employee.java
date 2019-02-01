5d3e9c665a87eda9b3bced84732879aec8fb6ef9
Sebastian Zillessen <sebastian@zillessen.info>
2019-02-01 15:43:16 +0100

    Add insight employees api with configuration - new EmployeeService - EmployeeDto for Insight - Insight Authentication (credentials via configuration need to be provided) - EmployeeRestController now returns data from the InsightApi - Application.properties contains placeholders for insight credentials

--- src/main/java/ch/zuehlke/fullstack/ConnectZuehlke/domain/Employee.java
+++ src/main/java/ch/zuehlke/fullstack/ConnectZuehlke/domain/Employee.java
@@ -1,4 +1,30 @@
 package ch.zuehlke.fullstack.ConnectZuehlke.domain;
 
 public class Employee {
+    private String firstName;
+    private String lastName;
+
+    private Employee() {
+    }
+
+    public Employee(String firstName, String lastName) {
+        setFirstName(firstName);
+        setLastName(lastName);
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    private void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    private void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 }
