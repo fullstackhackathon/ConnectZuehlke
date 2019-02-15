3fd541f85cc0603b4039bf59f366f3e566865f73
Sebastian Zillessen <sebastian@zillessen.info>
2019-02-15 09:43:46 +0100

    Remove and inline setters in Domain object

--- backend/src/main/java/ch/zuehlke/fullstack/ConnectZuehlke/domain/Employee.java
+++ backend/src/main/java/ch/zuehlke/fullstack/ConnectZuehlke/domain/Employee.java
@@ -15,41 +15,26 @@ public class Employee {
     }
 
     public Employee(String firstName, String lastName, int id, String code) {
-        setFirstName(firstName);
-        setLastName(lastName);
-        setId(id);
-        setCode(code);
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = id;
+        this.code = code.toLowerCase();
     }
 
     public String getLastName() {
         return lastName;
     }
 
-    private void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
     public String getFirstName() {
         return firstName;
-    }
-
-    private void setFirstName(String firstName) {
-        this.firstName = firstName;
     }
 
     public int getId() {
         return id;
     }
 
-    private void setId(int id) {
-        this.id = id;
-    }
-
     public String getCode() {
         return code;
     }
 
-    private void setCode(String code) {
-        this.code = code.toLowerCase();
-    }
 }
