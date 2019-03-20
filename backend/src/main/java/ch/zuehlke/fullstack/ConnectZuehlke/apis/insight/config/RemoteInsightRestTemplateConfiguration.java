9a128af003b520e786597e338579b9177797a9fb
Sebastian Zillessen <sebastian@zillessen.info>
2019-03-20 10:48:46 +0100

    Fixed problem with RemoteInsightRestTemplateConfiguration.java as it should not be loaded locally. Removed --debug flag

--- backend/src/main/java/ch/zuehlke/fullstack/ConnectZuehlke/apis/insight/config/RemoteInsightRestTemplateConfiguration.java
+++ backend/src/main/java/ch/zuehlke/fullstack/ConnectZuehlke/apis/insight/config/RemoteInsightRestTemplateConfiguration.java
@@ -17,11 +17,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.converter.ByteArrayHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@Profile({"staging", "prod"})
 public class RemoteInsightRestTemplateConfiguration {
     private Logger logger = LoggerFactory.getLogger(RemoteInsightRestTemplateConfiguration.class);
 
