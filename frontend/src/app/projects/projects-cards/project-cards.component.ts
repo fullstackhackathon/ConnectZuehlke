709595bbb856470ce4447f5d5ae973ac71109cd4
Stefan Friedli <stefan.friedli@zuehlke.com>
2019-03-21 09:51:14 +0100

    add card component

---
+++ frontend/src/app/projects/projects-cards/project-cards.component.ts
@@ -0,0 +1,20 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Project} from '../../shared/domain/Project';
+
+@Component({
+  selector: 'app-project-cards',
+  templateUrl: './project-cards.component.html',
+  styleUrls: ['./project-cards.component.scss']
+})
+export class ProjectCardsComponent {
+  @Input() projects: Project[];
+
+  @Output() projectClicked = new EventEmitter();
+
+  constructor() {
+  }
+
+  cardClicked(projectId: string) {
+    this.projectClicked.emit(projectId);
+  }
+}
--- frontend/src/app/projects/projects.module.ts
+++ frontend/src/app/projects/projects.module.ts
@@ -6,10 +6,13 @@ import {ProjectsListComponent} from './projects-list/projects-list.component';
 import {MatFormFieldModule, MatInputModule, MatListModule} from '@angular/material';
 import {FormsModule} from '@angular/forms';
 import {SharedModule} from '../shared/shared.module';
+import {ProjectCardsComponent} from './projects-cards/project-cards.component';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
-    ProjectsListComponent
+    ProjectsListComponent,
+    ProjectCardsComponent
   ],
   imports: [
     CommonModule,
@@ -18,7 +21,8 @@ import {SharedModule} from '../shared/shared.module';
     FormsModule,
     MatInputModule,
     MatFormFieldModule,
-    MatListModule
+    MatListModule,
+    MatCardModule
   ]
 })
 export class ProjectsModule {
