69f8cd5dc5a1a35c60332c317e05f5a708984552
Sebastian Zillessen <sebastian@zillessen.info>
2019-03-22 10:46:44 +0100

    Activate Lombok plugin for Employee.

--- backend/src/main/java/ch/zuehlke/fullstack/ConnectZuehlke/domain/Employee.java
+++ backend/src/main/java/ch/zuehlke/fullstack/ConnectZuehlke/domain/Employee.java
@@ -1,5 +1,13 @@
 package ch.zuehlke.fullstack.ConnectZuehlke.domain;
 
+import lombok.*;
+
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
 public class Employee {
 
     private String firstName;
@@ -7,34 +15,13 @@ public class Employee {
     private int id;
     private String code;
 
-    private Employee() {
+    public static Employee of(String firstName, String lastName, int id) {
+        String code = firstName.substring(0, 1) + lastName.substring(0, 2);
+        return Employee.builder()
+                .firstName(firstName)
+                .lastName(lastName)
+                .id(id)
+                .code(code)
+                .build();
     }
-
-    public Employee(String firstName, String lastName, int id) {
-        this(firstName, lastName, id, firstName.substring(0, 1) + lastName.substring(0, 2));
-    }
-
-    public Employee(String firstName, String lastName, int id, String code) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.id = id;
-        this.code = code.toLowerCase();
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
 }
