e31897b403617fd11cbe773c6ae2dfbe0b6e90d2
Sebastian Zillessen <sebastian@zillessen.info>
2019-03-22 10:38:43 +0100

    Activate Lombok plugin for Customer.

--- backend/src/main/java/ch/zuehlke/fullstack/ConnectZuehlke/domain/Customer.java
+++ backend/src/main/java/ch/zuehlke/fullstack/ConnectZuehlke/domain/Customer.java
@@ -1,7 +1,15 @@
 package ch.zuehlke.fullstack.ConnectZuehlke.domain;
 
-public class Customer {
 
+import lombok.*;
+
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+public class Customer {
     private Integer id;
     private String name;
     private String street;
@@ -10,48 +18,4 @@ public class Customer {
     private Integer zip;
     private Double longitude;
     private Double latitude;
-
-    public Customer(Integer id, String name, String street, String city, String country, Integer zip, Double longitude, Double latitude) {
-        this.id = id;
-        this.name = name;
-        this.street = street;
-        this.city = city;
-        this.country = country;
-        this.zip = zip;
-        this.longitude = longitude;
-        this.latitude = latitude;
-    }
-
-
-    public Integer getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public Integer getZip() {
-        return zip;
-    }
-
-    public Double getLongitude() {
-        return longitude;
-    }
-
-    public Double getLatitude() {
-        return latitude;
-    }
 }
